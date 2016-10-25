99d837afb0a40d9b5e16fb4771ceeaaacb42bad6
Glenn Spinner <glenngineer1@gmail.com>
2016-10-25 15:04:53 -0500

    refactor starting with express/server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+// middlewares
+app.use(express.static('client'))
+
+// Listen to requests on the provided port and log when available
+app.listen(port, () => console.log(`Listening on port: ${port}`))
+
