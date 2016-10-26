cfbe4af05befbc84061e5a56d5102bf1b33bd883
Glenn Spinner <glenngineer1@gmail.com>
2016-10-26 10:52:59 -0500

    added angular-route, partials folder and having routing working for splash and login page

--- client/app/main.js
+++ client/app/main.js
@@ -1,12 +1,24 @@
 'use strict'
 
 angular
-  .module('medicalRecordsTracker', [])
-  .controller('main', function ($scope, $http) {
+  .module('medicalRecordsTracker', ['ngRoute'])
+  .config($routeProvider =>
+    $routeProvider
+      .when('/', {
+        controller: 'SplashCtrl',
+        templateUrl: 'partials/splash.html',
+      })
+      .when('/login', {
+        controller: 'LoginCtrl',
+        templateUrl: 'partials/login.html',
+      })
+  )
+  .controller('SplashCtrl', function ($scope, $http) {
     $http
       .get('/api/title')
       .then(({ data: { title }}) =>
         $scope.title = title
       )
   })
+  .controller('LoginCtrl', function ($scope) {})
 
