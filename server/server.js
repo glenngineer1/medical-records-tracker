24db4eadb3f8d320c14bf980d4aa4ff10503908e
Glenn Spinner <glenngineer1@gmail.com>
2016-10-26 10:10:44 -0500

    getting title from node to DOM

--- server/server.js
+++ server/server.js
@@ -8,6 +8,10 @@ const port = process.env.PORT || 3000
 // middlewares
 app.use(express.static('client'))
 
+app.get('/api/title', (req, res) =>
+  res.json({ title: 'Medical Records Tracker' })
+)
+
 // Listen to requests on the provided port and log when available
  app.listen(port, () => console.log(`Listening on port: ${port}`))
 
